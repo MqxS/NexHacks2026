5eeb28794b5d8fe95a540c695a3c8c688396e632
MqxS <maxgoog06@outlook.com>
2026-01-17 14:46:53 -0500

    Added sample json

--- main.py
+++ main.py
@@ -6,6 +6,70 @@ server = Flask(__name__, static_folder="frontend/dist", static_url_path="")
 def hello():
     return jsonify({"message": "API Working!"})
 
+@server.route("/api/getClassCards")
+def get_class_cards():
+    class_cards = [
+        {"id": 1, "name": "Mathematics", "professor": "Dr. Karthik"},
+        {"id": 2, "name": "Science", "professor": "Dr. Joseph"},
+        {"id": 3, "name": "History", "professor": "Dr. Max"}
+    ]
+    return jsonify(class_cards)
+
+@server.route("/api/getClass")
+def crease_class():
+    classes = [
+        {"id": 1}
+    ]
+    return jsonify(classes)
+
+@server.route("/api/createSession")
+def create_session():
+    session = {
+        "sessionID": "ABC123",
+    }
+    return jsonify(session)
+
+@server.route("/api/getClassTopics/<classID>")
+def get_class_topics():
+    topics = [
+        {"title": "Algebra"},
+        {"title": "Geometry"},
+        {"title": "Calculus"}
+    ]
+    return jsonify(topics)
+
+@server.route("/api/requestQuestion/<sessionID>")
+def request_question():
+    question = {
+        "questionId": "Q123",
+        "content": "What is the capital of France?"
+    }
+    return jsonify(question)
+
+@server.route("/api/submitAnswer/<questionID>", methods=["POST"])
+def submit_answer():
+    response = {
+        "isCorrect": True,
+        "correctAnswer": "Paris",
+        "whyIsWrong": "Paris is the capital and most populous city of France.",
+    }
+    return jsonify(response)
+
+@server.route("/api/updateSessionParams/<sessionID>", methods=["POST"])
+def update_session_params():
+    return jsonify({"status": "Session parameters updated"})
+
+@server.route("/api/setAdaptive/<sessionID>", methods=["POST"])
+def set_adaptive():
+    return jsonify({"status": "Adaptive learning set"})
+
+@server.route("/api/requestHint/<questionID>")
+def request_hint():
+    hint = {
+        "hint": "It's also known as the city of lights."
+    }
+    return jsonify(hint)
+
 @server.route("/", defaults={"path": ""})
 @server.route("/<path:path>")
 def spa(path):
@@ -13,7 +77,6 @@ def spa(path):
         return jsonify({"error": "API route not found"}), 404
 
     return server.send_static_file("index.html")
-
 
 if __name__ == '__main__':
     server.run(port=8080)
